64c0ff583e39c8143e8925d8205d6e0816162521
Stanislav Ruzani <stanislav.ruzani@streamunlimited.com>
2019-01-12 16:44:23 +0100

    ASoC: meson: axg-tdm-interface: force alignment of audio buffer size to 2 bytes [STREAMEIGHT-2889]
    The problem was in the tdm audio driver on Stream1832 where the undocumented
    memory mapped register reporting the current audio reading pointer was not
    providing correct results when the size of audio buffer was not even number
    (aligned to 2).
    
    The DMA engine was able to handle this (thus there were no "obvious" dropouts
    caused by misaligned access in DMA engine) but the logic responsible for
    reporting the current audio buffer was off by 1 sample in case an odd buffer
    size was used. This was causing in long term incorrect reporting of audio
    playback latency -> audio time, and we were getting "drifing audio" problem,
    where the audio seemed to be "player faster" than it actually was.
    
    With this patch we can use any combination of period and buffer size within
    a range allowed by audio driver.
    
    Signed-off-by: Stanislav Ruzani <stanislav.ruzani@streamunlimited.com>
    [updated commit message with information from Jira]
    Signed-off-by: Radek Dostál <radek.dostal@streamunlimited.com>
    [rebased for 6.1]
    Signed-off-by: Peter Suti <peter.suti@streamunlimited.com>

--- sound/soc/meson/axg-tdm-interface.c
+++ sound/soc/meson/axg-tdm-interface.c
@@ -173,6 +173,13 @@ static int axg_tdm_iface_startup(struct snd_pcm_substream *substream,
 		ret = snd_pcm_hw_constraint_single(substream->runtime,
 						   SNDRV_PCM_HW_PARAM_RATE,
 						   iface->rate);
+		/*
+		* Make sure, that the buffer size is always even (aligned to 2 bytes)
+		* to avoid problems with audio pointer calculation!
+		*/
+		ret |= snd_pcm_hw_constraint_step(substream->runtime, 0,
+			SNDRV_PCM_HW_PARAM_BUFFER_SIZE, 2);
+
 		if (ret < 0) {
 			dev_err(dai->dev,
 				"can't set iface rate constraint\n");
