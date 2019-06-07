add87d619e7699ae3153fddd7d7e0db12b9750cc
Martin Pietryka <martin.pietryka@streamunlimited.com>
2019-06-07 15:45:56 +0200

    firmware: psci: add `disable-poweroff` device-tree binding for PSCI v0.2
    This will prevent the PSCI driver from registering a power-off handler. This
    can be useful if a different power-off hanlder (e.g. from a PMIC) will be
    used.
    
    Signed-off-by: Martin Pietryka <martin.pietryka@streamunlimited.com>
    [rebased for 6.1]
    Signed-off-by: Peter Suti <peter.suti@streamunlimited.com>

--- drivers/firmware/psci/psci.c
+++ drivers/firmware/psci/psci.c
@@ -590,7 +590,7 @@ static void __init psci_init_smccc(void)
 
 }
 
-static void __init psci_0_2_set_functions(void)
+static void __init psci_0_2_set_functions(const struct device_node *np)
 {
 	pr_info("Using standard PSCI v0.2 function IDs\n");
 
@@ -606,13 +606,14 @@ static void __init psci_0_2_set_functions(void)
 
 	register_restart_handler(&psci_sys_reset_nb);
 
-	pm_power_off = psci_sys_poweroff;
+	if (!of_property_read_bool(np, "disable-poweroff"))
+		pm_power_off = psci_sys_poweroff;
 }
 
 /*
  * Probe function for PSCI firmware versions >= 0.2
  */
-static int __init psci_probe(void)
+static int __init psci_probe(const struct device_node *np)
 {
 	u32 ver = psci_0_2_get_version();
 
@@ -625,7 +626,7 @@ static int __init psci_probe(void)
 		return -EINVAL;
 	}
 
-	psci_0_2_set_functions();
+	psci_0_2_set_functions(np);
 
 	psci_init_migrate();
 
@@ -662,7 +663,7 @@ static int __init psci_0_2_init(const struct device_node *np)
 	 * can be carried out according to the specific version reported
 	 * by firmware
 	 */
-	return psci_probe();
+	return psci_probe(np);
 }
 
 /*
@@ -767,6 +768,6 @@ int __init psci_acpi_init(void)
 	else
 		set_conduit(SMCCC_CONDUIT_SMC);
 
-	return psci_probe();
+	return psci_probe(NULL);
 }
 #endif
