793fb42b6fc0e5b6470ae5e753e54d2d555888ce
Martin Pietryka <martin.pietryka@streamunlimited.com>
2019-07-29 14:34:07 +0200

    ASoC: amlogic: auge: fix handling of MCLK rate
    The issue was that both the card driver and the TDM driver were setting the
    MCLK frequency. However the card driver was setting the frequency to
    rate*mclk_fs (mclk_fs was defined in the device-tree) and the TDM driver
    was setting the MCLK to either 48000*512 or 44100*512 (the behaviour we want).
    The issue was that for higher sample rates, the card driver would try to
    set the MCLK frequency first to some higher rate and then right after that,
    the TDM driver would set the MCLK to the correct frequency. This meant that
    for a short amount of time the frequency was wrong, this could lead to audible
    audio artifacts.
    
    This patch fixes this behaviour:
    * Only set the MCLK rate in the TDM driver if it was not already set by
      the card driver
    * If mclk-fs is defined in the device-tree use this factor
    * Otherwise use one of two fixed values (48000*512) or (44100*512)
    
    Signed-off-by: Martin Pietryka <martin.pietryka@streamunlimited.com>

--- sound/soc/amlogic/auge/clocks.h
+++ sound/soc/amlogic/auge/clocks.h
@@ -4,4 +4,6 @@
 unsigned int aml_mpll_mclk_ratio(unsigned int freq);
 unsigned int aml_hifipll_mclk_ratio(unsigned int freq);
 
+unsigned int aml_get_mclk_rate(unsigned int rate);
+
 #endif /* __AML_AUDIO_CLOCKS_H_ */
