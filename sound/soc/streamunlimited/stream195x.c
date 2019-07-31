ae6c9304837e35e6be1e60d2f123617e523c5d2d
Martin Pietryka <martin.pietryka@streamunlimited.com>
2019-07-31 11:30:43 +0200

    ASoC: stream195x: add Stream195x sound card
    This card driver allows to specify dai links using the device-tree,
    similar to simple-card. However it has some features where it does not
    make sense to add them to the current simple-card.
    
    * Support for the `Drift compensator` mixer element. This mixer element
      is used by the userspace to change the audio clock frequency by up to
      +-500 ppm. This is required in some cases where the user-space wants
      to synchronize audio without dropping/inserting samples.
    
    * Support for a powerdown GPIO and a per-dai-link mute GPIO. Currently
      all those GPIOs are enabled on probe and disabled on remove. However
      this allows us to extend the driver in the future to allow fine
      grained control for multiple mute lines for example.
    
    * Possibility to specify `use-dummy-codec` on the codec node of the dai
      link. Many devices and customers do not implement a proper codec
      driver. For previous platforms we have written a dummy codec driver
      that allows all formats and rates. However the ASoC core has already
      such a driver builtin and this flag allows us to use this dummy driver
      instead of a real codec driver.
    
    * Extensibility, in the future we will require support for features
      like DSD and multi-channel audio. Having our own card driver allows us
      to be very flexible in that regard.
    
    Signed-off-by: Martin Pietryka <martin.pietryka@streamunlimited.com>
    [fix bracket symmetry]
    Signed-off-by: Radek Dostál <radek.dostal@streamunlimited.com>

---
+++ sound/soc/streamunlimited/stream195x.c
@@ -0,0 +1,250 @@
+#include <linux/clk.h>
+#include <linux/clk-provider.h>
+#include <linux/gpio/consumer.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+
+#include <sound/simple_card.h>
+
+#include "stream195x.h"
+
+#include "../fsl/fsl_sai.h"
+
+#define MCLK_RATE_48k	(24576000UL)
+#define MCLK_RATE_44k1	(22579200UL)
+
+#define PLL_NOMINAL_RATE_48k	(786432000UL)
+#define PLL_NOMINAL_RATE_44k1	(722534400UL)
+
+static int snd_soc_stream195x_ppm_info(struct snd_kcontrol *kcontrol, struct snd_ctl_elem_info *uinfo)
+{
+	uinfo->type = SNDRV_CTL_ELEM_TYPE_INTEGER;
+	uinfo->value.integer.min = -500;
+	uinfo->value.integer.max = 500;
+	uinfo->count = 1;
+
+	return 0;
+}
+
+static int snd_soc_stream195x_ppm_get(struct snd_kcontrol *kcontrol, struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_card *card = snd_kcontrol_chip(kcontrol);
+	struct snd_soc_stream195x_data *priv = snd_soc_card_get_drvdata(card);
+
+	ucontrol->value.integer.value[0] = priv->cur_ppm;
+
+	return 0;
+}
+
+static int snd_soc_stream195x_ppm_put(struct snd_kcontrol *kcontrol, struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_card *card = snd_kcontrol_chip(kcontrol);
+	struct snd_soc_stream195x_data *priv = snd_soc_card_get_drvdata(card);
+
+	int ret;
+	unsigned int comp;
+	unsigned int clk_rate;
+	int ppm = ucontrol->value.integer.value[0];
+
+	comp = DIV_ROUND_CLOSEST_ULL((u64)PLL_NOMINAL_RATE_48k * abs(ppm), 1000000UL);
+	clk_rate = PLL_NOMINAL_RATE_48k;
+	if (ppm > 0)
+		clk_rate += comp;
+	else
+		clk_rate -= comp;
+
+	ret = clk_set_rate(priv->pll8k_clk, clk_rate);
+	if (ret)
+		dev_warn(card->dev, "failed to set pll8k rate %d\n", ret);
+
+
+	comp = DIV_ROUND_CLOSEST_ULL((u64)PLL_NOMINAL_RATE_44k1 * abs(ppm), 1000000UL);
+	clk_rate = PLL_NOMINAL_RATE_44k1;
+	if (ppm > 0)
+		clk_rate += comp;
+	else
+		clk_rate -= comp;
+
+	ret = clk_set_rate(priv->pll11k_clk, clk_rate);
+	if (ret)
+		dev_warn(card->dev, "failed to set pll11k rate %d\n", ret);
+
+	priv->cur_ppm = ppm;
+
+	return 1;
+}
+
+static const struct snd_kcontrol_new snd_soc_stream195x_controls[] = {
+	{
+		.name = "Drift compensator",
+		.iface = SNDRV_CTL_ELEM_IFACE_MIXER,
+		.info = snd_soc_stream195x_ppm_info,
+		.get = snd_soc_stream195x_ppm_get,
+		.put = snd_soc_stream195x_ppm_put,
+	},
+};
+
+static int snd_soc_stream195x_hw_params(struct snd_pcm_substream *substream, struct snd_pcm_hw_params *params)
+{
+	int ret;
+	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_stream195x_data *priv = snd_soc_card_get_drvdata(rtd->card);
+	struct snd_soc_dai *codec_dai = rtd->codec_dai;
+	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
+	struct snd_soc_stream195x_dai_link_data *dai_link_data = priv->dai_link_data + rtd->num;
+
+	unsigned int rate = params_rate(params);
+	unsigned int mclk_rate, pll_rate;
+	struct clk *pll;
+
+	if ((rate % 8000) == 0) {
+		pll_rate = PLL_NOMINAL_RATE_48k;
+		pll = priv->pll8k_clk;
+		mclk_rate = MCLK_RATE_48k;
+	} else {
+		pll_rate = PLL_NOMINAL_RATE_44k1;
+		pll = priv->pll11k_clk;
+		mclk_rate = MCLK_RATE_44k1;
+	}
+
+	/*
+	 * We could have done the tdm slot setup only once, and not in every hw_params()
+	 * call, however sooner or later we will support DSD where the tdm slot setup
+	 * will change at runtime, so let's just add it here.
+	 */
+	ret = snd_soc_dai_set_tdm_slot(codec_dai, 0, 0, dai_link_data->slots, dai_link_data->slot_width);
+	if (ret && ret != -ENOTSUPP)
+		return ret;
+
+	ret = snd_soc_dai_set_tdm_slot(cpu_dai, 0, 0, dai_link_data->slots, dai_link_data->slot_width);
+	if (ret && ret != -ENOTSUPP)
+		return ret;
+
+
+	/*
+	 * First we reset the PLL rate to the nominal value, otherwise, when
+	 * the PLL is in a state where the frequency is skewed, the
+	 * calculations inside snd_soc_dai_set_sysclk() might fail, because
+	 * the skewed PLL value cannot be cleanly divided down anymore.
+	 */
+	priv->cur_ppm = 0;
+	ret = clk_set_rate(pll, pll_rate);
+	if (ret)
+		return ret;
+
+	ret = snd_soc_dai_set_sysclk(codec_dai, 0, mclk_rate, SND_SOC_CLOCK_IN);
+	if (ret && ret != -ENOTSUPP)
+		return ret;
+
+	/* TODO: make the id parameter configurable via the device-tree */
+	ret = snd_soc_dai_set_sysclk(cpu_dai, FSL_SAI_CLK_MAST1, mclk_rate, SND_SOC_CLOCK_OUT);
+	if (ret && ret != -ENOTSUPP)
+		return ret;
+
+
+	return 0;
+}
+
+static const struct snd_soc_ops snd_soc_stream195x_ops = {
+	.hw_params = snd_soc_stream195x_hw_params,
+};
+
+static void snd_soc_stream195x_set_powerdown(struct snd_soc_stream195x_data *priv, int value)
+{
+	if (priv->powerdown_gpio)
+		gpiod_set_value(priv->powerdown_gpio, value);
+}
+
+static void snd_soc_stream195x_set_all_links_mute(struct snd_soc_stream195x_data *priv, int value)
+{
+	int i;
+	struct snd_soc_card *card = &priv->card;
+
+	for (i = 0; i < card->num_links; i++) {
+		struct snd_soc_stream195x_dai_link_data *dai_link_data = priv->dai_link_data + i;
+		if (dai_link_data->mute_gpio)
+			gpiod_set_value_cansleep(dai_link_data->mute_gpio, value);
+	}
+}
+
+static int snd_soc_stream195x_probe(struct platform_device *pdev)
+{
+	int ret;
+	struct device *dev = &pdev->dev;
+	struct snd_soc_stream195x_data *priv;
+
+	priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
+	if (!priv)
+		return -ENOMEM;
+
+	priv->dev = dev;
+
+	ret = snd_soc_stream195x_parse_of(priv, &snd_soc_stream195x_ops);
+	if (ret < 0) {
+		dev_err(dev, "failed to parse device-tree\n");
+		goto exit;
+	}
+
+	snd_soc_card_set_drvdata(&priv->card, priv);
+
+	priv->card.controls = snd_soc_stream195x_controls;
+	priv->card.num_controls = ARRAY_SIZE(snd_soc_stream195x_controls);
+
+	/*
+	 * Power up the card before calling register. There might be cases
+	 * when the registing and probing of the components will fail if the
+	 * powerdown is enabled
+	 */
+	snd_soc_stream195x_set_powerdown(priv, 0);
+
+	ret = devm_snd_soc_register_card(dev, &priv->card);
+	if (ret < 0) {
+		dev_err(dev, "failed to register card");
+		goto exit;
+	}
+
+	/* Unmute all links */
+	snd_soc_stream195x_set_all_links_mute(priv, 0);
+
+exit:
+	/* In the error case put all cpu and codec of nodes */
+	if (ret != 0) {
+		snd_soc_stream195x_set_all_links_mute(priv, 1);
+		snd_soc_stream195x_set_powerdown(priv, 1);
+		asoc_simple_card_clean_reference(&priv->card);
+	}
+
+	return ret;
+}
+
+static int snd_soc_stream195x_remove(struct platform_device *pdev)
+{
+	struct snd_soc_card *card = platform_get_drvdata(pdev);
+	struct snd_soc_stream195x_data *priv = snd_soc_card_get_drvdata(card);
+
+	snd_soc_stream195x_set_all_links_mute(priv, 1);
+	snd_soc_stream195x_set_powerdown(priv, 1);
+
+	return asoc_simple_card_clean_reference(card);
+}
+
+static const struct of_device_id snd_soc_stream195x_match[] = {
+	{ .compatible	= "sue,stream195x-audio" },
+	{ }
+};
+
+static struct platform_driver snd_soc_stream195x_driver = {
+	.driver = {
+		.owner		= THIS_MODULE,
+		.name		= "snd-soc-stream195x",
+		.of_match_table	= snd_soc_stream195x_match,
+	},
+	.probe		= snd_soc_stream195x_probe,
+	.remove		= snd_soc_stream195x_remove,
+};
+module_platform_driver(snd_soc_stream195x_driver);
+
+MODULE_AUTHOR("Martin Pietryka <martin.pietryka@streamunlimited.com>");
+MODULE_DESCRIPTION("Stream Unlimited Stream195x ASoC driver");
+MODULE_LICENSE("GPL");
+MODULE_ALIAS("platform:snd-soc-stream195x");
---
+++ sound/soc/streamunlimited/stream195x_of.c
@@ -0,0 +1,171 @@
+#include <linux/of.h>
+#include <linux/of_gpio.h>
+#include <linux/platform_device.h>
+#include <linux/gpio/machine.h>
+#include <linux/clk.h>
+#include <linux/clk-provider.h>
+
+#include <sound/simple_card.h>
+
+#include "stream195x.h"
+
+#define PREFIX	"sue-card,"
+
+#define DAI	"sound-dai"
+#define CELL	"#sound-dai-cells"
+
+/*
+ * This is a wrapper around devm_gpiod_get_from_of_node() to assemble
+ * a nice gpio label based on dai_link->codec_dai_name.
+ */
+static struct gpio_desc *devm_dailink_gpiod_get_from_of_node(struct device *dev, struct snd_soc_dai_link *dai_link,
+								struct device_node *np, const char *name, enum gpiod_flags flags)
+{
+	char buf[32], *gpio_label;
+
+	snprintf(buf, sizeof(buf), "%s-%s", dai_link->codec_dai_name, name);
+	gpio_label = devm_kstrdup(dev, buf, GFP_KERNEL);
+
+	return devm_gpiod_get_from_of_node(dev, np, name, 0, flags, gpio_label);
+}
+
+int snd_soc_stream195x_parse_dai_link_of(struct snd_soc_stream195x_data *priv, int index,
+						struct device_node *np, const struct snd_soc_ops *link_ops)
+{
+	int ret;
+	struct device *dev = priv->dev;
+	struct device_node *cpu = NULL;
+	struct device_node *codec = NULL;
+
+	struct snd_soc_dai_link *dai_link = priv->dai_links + index;
+	struct snd_soc_stream195x_dai_link_data *dai_link_data = priv->dai_link_data + index;
+	int single_cpu;
+
+	cpu = of_get_child_by_name(np, "cpu");
+	if (!cpu) {
+		dev_err(dev, "%s: Can't find cpu DT node\n", __func__);
+		ret = -EINVAL;
+		goto exit;
+	}
+
+	codec = of_get_child_by_name(np, "codec");
+	if (!codec) {
+		dev_err(dev, "%s: Can't find codec DT node\n", __func__);
+		ret = -EINVAL;
+		goto exit;
+	}
+
+	ret = asoc_simple_card_parse_daifmt(dev, np, codec, "", &dai_link->dai_fmt);
+	if (ret < 0)
+		goto exit;
+
+	ret = snd_soc_of_parse_tdm_slot(np, NULL, NULL, &dai_link_data->slots, &dai_link_data->slot_width);
+	if (ret < 0)
+		goto exit;
+
+	ret = asoc_simple_card_parse_cpu(cpu, dai_link, DAI, CELL, &single_cpu);
+	if (ret < 0)
+		goto exit;
+
+	if (of_property_read_bool(codec, "use-dummy-codec")) {
+		dai_link->codec_dai_name = "snd-soc-dummy-dai";
+		dai_link->codec_name = "snd-soc-dummy";
+	} else if (asoc_simple_card_parse_codec(codec, dai_link, DAI, CELL) < 0) {
+		goto exit;
+	}
+
+	ret = asoc_simple_card_canonicalize_dailink(dai_link);
+	if (ret < 0)
+		goto exit;
+
+	ret = asoc_simple_card_set_dailink_name(dev, dai_link,
+						"%s-%s",
+						dai_link->cpu_dai_name,
+						dai_link->codec_dai_name);
+	if (ret < 0)
+		goto exit;
+
+
+	dai_link_data->mute_gpio = devm_dailink_gpiod_get_from_of_node(dev, dai_link, np, "mute-gpios", GPIOD_OUT_HIGH);
+	if (IS_ERR(dai_link_data->mute_gpio))
+		dai_link_data->mute_gpio = NULL;
+
+	dai_link->ops = link_ops;
+	/* dai_link->init = asoc_simple_card_dai_init; */
+
+	asoc_simple_card_canonicalize_cpu(dai_link, single_cpu);
+exit:
+	of_node_put(cpu);
+	of_node_put(codec);
+
+	return ret;
+}
+
+int snd_soc_stream195x_parse_of(struct snd_soc_stream195x_data *priv, const struct snd_soc_ops *link_ops)
+{
+	int ret = 0;
+	int i;
+	struct device *dev = priv->dev;
+	struct device_node *of = dev->of_node;
+	struct device_node *dai_link, *np;
+
+	struct snd_soc_card *card;
+	int num_links;
+	struct snd_soc_dai_link *dai_links;
+	struct snd_soc_stream195x_dai_link_data *dai_link_data;
+
+	/* TODO: should we return an error if no clock is specified? */
+	priv->pll8k_clk = devm_clk_get(dev, "pll8k");
+	if (IS_ERR(priv->pll8k_clk))
+		priv->pll8k_clk = NULL;
+
+	priv->pll11k_clk = devm_clk_get(dev, "pll11k");
+	if (IS_ERR(priv->pll11k_clk))
+		priv->pll11k_clk = NULL;
+
+	dai_link = of_get_child_by_name(of, PREFIX "dai-link");
+	if (!dai_link) {
+		dev_err(dev, "could not find dai-link nodes\n");
+		return -EINVAL;
+	}
+
+	card = &priv->card;
+	num_links = of_get_child_count(of);
+
+	dai_link_data = devm_kzalloc(dev, sizeof(*dai_link_data) * num_links, GFP_KERNEL);
+	dai_links  = devm_kzalloc(dev, sizeof(*dai_links) * num_links, GFP_KERNEL);
+	if (!dai_link_data || !dai_links) {
+		ret = -ENOMEM;
+		goto exit_put_dai_link;
+	}
+
+	priv->dai_link_data = dai_link_data;
+	priv->dai_links = dai_links;
+
+	card->owner = THIS_MODULE;
+	card->dev = dev;
+	card->dai_link = dai_links;
+	card->num_links = num_links;
+
+	i = 0;
+	for_each_child_of_node(of, np) {
+		ret = snd_soc_stream195x_parse_dai_link_of(priv, i, np, link_ops);
+		i++;
+
+		if (ret < 0) {
+			dev_err(dev, "failed to parse dai-link\n");
+			of_node_put(np);
+			goto exit_put_dai_link;
+		}
+	}
+
+	ret = asoc_simple_card_parse_card_name(card, PREFIX);
+	if (ret < 0)
+		goto exit_put_dai_link;
+
+	priv->powerdown_gpio = devm_gpiod_get_optional(dev, "powerdown", GPIOD_OUT_HIGH);
+
+exit_put_dai_link:
+	of_node_put(dai_link);
+	return ret;
+}
