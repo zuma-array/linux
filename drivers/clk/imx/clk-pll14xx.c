b337ef4507085f1a2afa334bc7d51ba5c61e2d8c
Martin Pietryka <martin.pietryka@streamunlimited.com>
2019-08-12 16:00:40 +0200

    clk: imx8mm: use skewable pll for the audio pll
    This will allow us to change the frequency of the PLL arbitrary around the
    defined value.
    
    Signed-off-by: Martin Pietryka <martin.pietryka@streamunlimited.com>

--- drivers/clk/imx/clk-pll14xx.c
+++ drivers/clk/imx/clk-pll14xx.c
@@ -61,12 +61,12 @@ static const struct imx_pll14xx_rate_table imx_pll1443x_tbl[] = {
 	PLL_1443X_RATE(650000000U, 325, 3, 2, 0),
 	PLL_1443X_RATE(594000000U, 198, 2, 2, 0),
 	PLL_1443X_RATE(519750000U, 173, 2, 2, 16384),
-	PLL_1443X_RATE(393216000U, 262, 2, 3, 9437),
-	PLL_1443X_RATE(361267200U, 361, 3, 3, 17511),
+	PLL_1443X_RATE(722534400U, 301, 5, 1, 0xE56),	/* 32 * MCLK for 44.1 kHz */
+	PLL_1443X_RATE(786432000U, 328, 5, 1, 0xAE14),	/* 32 * MCLK for 48 kHz */
 };
 
 struct imx_pll14xx_clk imx_1443x_pll = {
-	.type = PLL_1443X,
+	.type = PLL_1443X_SKEWABLE,
 	.rate_table = imx_pll1443x_tbl,
 	.rate_count = ARRAY_SIZE(imx_pll1443x_tbl),
 };
