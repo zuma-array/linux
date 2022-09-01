e03dc428682746e72496ec8b7258de922574f813
Peter Suti <peter.suti@streamunlimited.com>
2022-09-01 14:01:17 +0200

    watchdog: meson: enable watchdog in probe
    Signed-off-by: Peter Suti <peter.suti@streamunlimited.com>

--- drivers/watchdog/meson_gxbb_wdt.c
+++ drivers/watchdog/meson_gxbb_wdt.c
@@ -211,6 +211,7 @@ static int meson_gxbb_wdt_probe(struct platform_device *pdev)
 
 	writel(ctrl_reg, data->reg_base + GXBB_WDT_CTRL_REG);
 	meson_gxbb_wdt_set_timeout(&data->wdt_dev, data->wdt_dev.timeout);
+	meson_gxbb_wdt_start(&data->wdt_dev);
 
 	dev_info(dev, "Watchdog enabled (timeout=%d sec, nowayout=%d)",
 		 data->wdt_dev.timeout, nowayout);
