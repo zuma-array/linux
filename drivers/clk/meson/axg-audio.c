af5616ce0bf04a39bd349066ddf51885e6f88244
Peter Suti <peter.suti@streamunlimited.com>
2022-09-19 09:38:20 +0200

    clk: meson: axg-audio: enable SET_RATE_PARENT only for mst_mclk
    We run every audio clock from the HIFI_PLL, which includes the PDM and the
    TDM MCLK. When the PDM sets the rate, it should not recalculate the
    HIFI_PLL because that is set by the drift-compensator by one of the TDM
    channels.
    This commit introduces a new construct, called
    AUD_MST_MCLK_MUX_SET_PARENT which is only used for mst_mclk and not for
    TDM. This way when the PDM driver sets the rate for the microphone the
    HIFI_PLL is not disturbed.
    
    Signed-off-by: Peter Suti <peter.suti@streamunlimited.com>

--- drivers/clk/meson/axg-audio.c
+++ drivers/clk/meson/axg-audio.c
@@ -182,9 +182,12 @@ static const struct clk_parent_data mst_mux_parent_data[] = {
 	{ .fw_name = "mst_in7", },
 };
 
-#define AUD_MST_MUX(_name, _reg, _flag)					\
+#define AUD_MST_MUX_SET_PARENT(_name, _reg, _flag)					\
 	AUD_MUX(_name##_sel, _reg, 0x7, 24, _flag,			\
 		mst_mux_parent_data, CLK_SET_RATE_NO_REPARENT | CLK_SET_RATE_PARENT)
+#define AUD_MST_MUX(_name, _reg, _flag)					\
+	AUD_MUX(_name##_sel, _reg, 0x7, 24, _flag,			\
+		mst_mux_parent_data, CLK_SET_RATE_NO_REPARENT)
 #define AUD_MST_DIV(_name, _reg, _flag)					\
 	AUD_DIV(_name##_div, _reg, 0, 16, _flag,			\
 		aud_##_name##_sel, CLK_SET_RATE_PARENT)
@@ -192,6 +195,8 @@ static const struct clk_parent_data mst_mux_parent_data[] = {
 	AUD_GATE(_name, _reg, 31, aud_##_name##_div,			\
 		 CLK_SET_RATE_PARENT)
 
+#define AUD_MST_MCLK_MUX_SET_PARENT(_name, _reg)					\
+	AUD_MST_MUX_SET_PARENT(_name, _reg, CLK_MUX_ROUND_CLOSEST)
 #define AUD_MST_MCLK_MUX(_name, _reg)					\
 	AUD_MST_MUX(_name, _reg, CLK_MUX_ROUND_CLOSEST)
 #define AUD_MST_MCLK_DIV(_name, _reg)					\
@@ -564,17 +569,17 @@ static struct clk_hw axg_aud_top = {
 };
 
 static struct clk_regmap mst_a_mclk_sel =
-	AUD_MST_MCLK_MUX(mst_a_mclk, AUDIO_MCLK_A_CTRL);
+	AUD_MST_MCLK_MUX_SET_PARENT(mst_a_mclk, AUDIO_MCLK_A_CTRL);
 static struct clk_regmap mst_b_mclk_sel =
-	AUD_MST_MCLK_MUX(mst_b_mclk, AUDIO_MCLK_B_CTRL);
+	AUD_MST_MCLK_MUX_SET_PARENT(mst_b_mclk, AUDIO_MCLK_B_CTRL);
 static struct clk_regmap mst_c_mclk_sel =
-	AUD_MST_MCLK_MUX(mst_c_mclk, AUDIO_MCLK_C_CTRL);
+	AUD_MST_MCLK_MUX_SET_PARENT(mst_c_mclk, AUDIO_MCLK_C_CTRL);
 static struct clk_regmap mst_d_mclk_sel =
-	AUD_MST_MCLK_MUX(mst_d_mclk, AUDIO_MCLK_D_CTRL);
+	AUD_MST_MCLK_MUX_SET_PARENT(mst_d_mclk, AUDIO_MCLK_D_CTRL);
 static struct clk_regmap mst_e_mclk_sel =
-	AUD_MST_MCLK_MUX(mst_e_mclk, AUDIO_MCLK_E_CTRL);
+	AUD_MST_MCLK_MUX_SET_PARENT(mst_e_mclk, AUDIO_MCLK_E_CTRL);
 static struct clk_regmap mst_f_mclk_sel =
-	AUD_MST_MCLK_MUX(mst_f_mclk, AUDIO_MCLK_F_CTRL);
+	AUD_MST_MCLK_MUX_SET_PARENT(mst_f_mclk, AUDIO_MCLK_F_CTRL);
 
 static struct clk_regmap mst_a_mclk_div =
 	AUD_MST_MCLK_DIV(mst_a_mclk, AUDIO_MCLK_A_CTRL);
