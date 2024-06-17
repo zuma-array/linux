3d463568a1221c988187154152da7945e8df8415
Simon Mikuda <simon.mikuda@streamunlimited.com>
2024-06-17 08:11:18 +0200

    bluetooth: allow simultaneous central and peripheral connections
    This allows use case:
    * we are advertising and scanning
    * remote device connects to us on advertisement
    * we've received directed advertisement for us
    * we connect to advertisement
    
    Signed-off-by: Simon Mikuda <simon.mikuda@streamunlimited.com>
    [delete the code instead just commenting it out]
    Signed-off-by: Radek Dostál <radek.dostal@streamunlimited.com>

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -6107,14 +6107,6 @@ static struct hci_conn *check_pending_le_conn(struct hci_dev *hdev,
 	    hdev->suspended)
 		return NULL;
 
-	/* Most controller will fail if we try to create new connections
-	 * while we have an existing one in peripheral role.
-	 */
-	if (hdev->conn_hash.le_num_peripheral > 0 &&
-	    (!test_bit(HCI_QUIRK_VALID_LE_STATES, &hdev->quirks) ||
-	     !(hdev->le_states[3] & 0x10)))
-		return NULL;
-
 	/* If we're not connectable only connect devices that we have in
 	 * our pend_le_conns list.
 	 */
